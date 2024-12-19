de7ebd98f829d9738f1bee0cdc35666725e4ae68
scvit <88419463+scvit@users.noreply.github.com>
2024-12-19 13:47:28 +0900

    Update main.tf

--- main.tf
+++ main.tf
@@ -45,7 +45,7 @@ resource "aws_instance" "custom_ami_ec2" {
 
     delete_on_termination = true
     volume_type           = "gp3" #each.value.ec2_root_volume.ec2_root_volume_type
-    volume_size           = "25" #each.value.ec2_root_volume.ec2_root_volume_size
+    volume_size           = "30" #each.value.ec2_root_volume.ec2_root_volume_size
     iops                  = 3000 #contains(["io1", "io2", "gp3"], each.value.ec2_root_volume.ec2_root_volume_type) && each.value.ec2_root_volume.ec2_root_volume_iops != "" ? each.value.ec2_root_volume.ec2_root_volume_iops : null
     throughput            = 125 #contains(["gp3"], each.value.ec2_root_volume.ec2_root_volume_type) && each.value.ec2_root_volume.ec2_root_volume_throughput != "" ? each.value.ec2_root_volume.ec2_root_volume_throughput : null
 
